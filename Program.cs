d0fae9dcc435773cddea82243a03a872dd0ce8c0
Michele Andersson <michele.andersson@ikkyu.se>
2015-12-06 22:55:46 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -14,25 +14,27 @@ namespace TicTacToe
             
             do
             {
-                game.NextPlayer();
-                game.DrawBoard();
-                Console.Write($"Player {game.PlayerToken} Move: ");
-                game.PlayerMove();
+                game.NextPlayer(); // Check who plays next and set that players token X || O
+                game.DrawBoard(); // Draw the game board
+                Console.Write($"Player {game.PlayerToken} Move: "); 
+                game.PlayerMove(); // Ask the player for input [1-9] and place token on that square.
 
-                if (game.CheckWin())
+                if (game.CheckWin()) // After each turn check if someone won the game
                 {
-                    game.DrawBoard();
+                    game.DrawBoard(); // Draw the board again
                     Console.WriteLine($"Player {game.PlayerToken} wins!");
-                    game.Over = true;
+                    game.Over = true; 
                 }
-                else if (game.Turn == 9)
+                // If no one won, check if there been 9 turns. 
+                // If so all empty slots on the boards are full and its a draw
+                else if (game.Turn == 9) 
                 {
                     game.DrawBoard();
                     Console.WriteLine("It's a Draw!");
                     game.Over = true;
                 }
             }
-            while (!game.Over);
+            while (!game.Over); // Continue as long as the game is not won or a draw.
         }
 
     }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -8,20 +8,27 @@ namespace TicTacToe
 {
     class TicTacToe
     {
-        public string PlayerToken { get; set; }
-        public bool Over { get; set; }
-        public int Turn { get; set; }
+        // create properties
+        public string PlayerToken { get; set; } // used to keep the playertoken X || O
+        public bool Over { get; set; } // hold the value for if the game is over or not
+        public int Turn { get; set; } // keeps the number of turns played 
 
+        // create the 2 dimensional array to hold the game board
         string[,] board = new string[3,3];
         
+        // Constructor to create the game board
         public TicTacToe()
         {
             board = new string[,] { { "1", "2", "3" }, 
                                     { "4", "5", "6" }, 
                                     { "7", "8", "9" }};
-            PlayerToken = "O";
+            // I set the token to O at creation but then at start its set to X.
+            // I was struggling to find a better way to set so that X always started and that
+            // the players took turn afterwards. 
+            PlayerToken = "O"; 
         }
 
+        // Draw out the game board on screen. Showing all empty and taken slots.
         public void DrawBoard()
         {
             Console.Clear();
@@ -40,30 +47,43 @@ namespace TicTacToe
             Console.WriteLine();
         }
 
+        // Let the player make a move
         public void PlayerMove()
         {
             bool answer = false;
 
+            // loop until the player gives a valid input
             while (!answer)
             {
+                // I used ReadKey(true) to minimize the keypresses needed to make a move. 
+                // (true) also to prevent the keystroke to be written to screen
                 string playerChoice = Console.ReadKey(true).KeyChar.ToString();
 
+                // I was thinking of naming the variables to row & column. 
+                // But read that the namestandard for 2 dimensional arrays are i, j
+                // So i went with that instead. 
+
+                // Loop thru each row
                 for (int i = 0; i < 3; i++)
                 {
+                    // Loop thru each column
                     for (int j = 0; j < 3; j++)
                     {
+                        // If the players choice match the value on that slot and is not taken already by X or O
                         if (board[i, j] == playerChoice && (board[i, j] != "X" || board[i, j] != "O"))
                         {
-                            board[i, j] = PlayerToken;
-                            Turn++;
-                            answer = true;
-                            return;
+                            board[i, j] = PlayerToken; // Set the token to the current player's
+                            Turn++; // Increment the turn (counter runs up to 9 which mean a draw occured)
+                            answer = true; 
+                            return; // break out of the method and go back to the game.
                         }                        
                     }
                 }
-
+                // If the user fails to make a valid choice.
                 if (!answer)
                 {
+                    // Draw the board again and write out a red message 
+                    // Asking the user to make a new choice
                     DrawBoard();
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Invalid move!");
@@ -74,6 +94,8 @@ namespace TicTacToe
 
         }
 
+        // Check who is playing now if its the current player, change to the other one.
+        // This is why i started with player O in the constructor, it gets changed to X and after the loop works.
         public void NextPlayer()
         {
             if (PlayerToken == "O")
@@ -83,6 +105,7 @@ namespace TicTacToe
             else PlayerToken = "O";
         }
 
+        // Method to check if the player made a winning move
         public bool CheckWin()
         {
             // Check diagonally from upper left corner to lower right
